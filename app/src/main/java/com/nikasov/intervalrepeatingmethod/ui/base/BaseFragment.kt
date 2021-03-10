13d2aec1f86618ccf6cc408f1ed363981cdb6d9f
mishanikasov <mishanikasov@mail.ru>
2021-03-10 15:43:32 +0200

    binding to base fragment

--- app/src/main/java/com/nikasov/intervalrepeatingmethod/ui/base/BaseFragment.kt
+++ app/src/main/java/com/nikasov/intervalrepeatingmethod/ui/base/BaseFragment.kt
@@ -1,10 +1,12 @@
 package com.nikasov.intervalrepeatingmethod.ui.base
 
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import com.nikasov.intervalrepeatingmethod.common.extentions.showToast
 import java.lang.Exception
 
-open class BaseFragment : Fragment() {
+abstract class BaseFragment<Binding: ViewDataBinding> : Fragment() {
+    lateinit var binding: Binding
     fun displayError(exception: Exception) {
         requireContext().showToast(exception.localizedMessage ?: "Error")
     }
